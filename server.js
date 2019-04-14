b2d188d43e148c7d5d93596ddbce38a73d4851d0
WillyD <sopwill3@gmail.com>
2019-04-14 17:34:02 -0600

    next two steps done

--- server.js
+++ server.js
@@ -1,14 +1,19 @@
 const express = require('express');
 const app = express();
-
+const pokemon = require('./modules/pokemon');
 
 
 
 app.get('/pokemon', function(req,res){
-
+  res.render('index.ejs',{
+    pokemon: pokemon
+  });
 })
 app.get('/pokemon/:id', function(req,res){
-
+  res.render('show.ejs',{
+    pokemon: pokemon,
+    i: req.params.id
+  });
 })
 
 
