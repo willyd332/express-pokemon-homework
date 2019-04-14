1af55264f34e0c074674f4de8cb8267c39db9a85
WillyD <sopwill3@gmail.com>
2019-04-14 17:24:21 -0600

    server up

---
+++ server.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+
+
+
+
+app.get('/pokemon', function(req,res){
+
+})
+app.get('/pokemon/:id', function(req,res){
+
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+const port = 3000
+app.listen(port, function() {
+  console.log('servers up');
+})
