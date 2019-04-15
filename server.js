9efff5c57feff42071e632c013c7a8a901d0b8e1
WillyD <sopwill3@gmail.com>
2019-04-14 18:41:39 -0600

    creates post and delete routes

--- server.js
+++ server.js
@@ -1,14 +1,21 @@
 const express = require('express');
 const app = express();
 const pokemon = require('./modules/pokemon');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
 
 app.use(express.static('./public'))
-
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(methodOverride('_method'))
 
 app.get('/pokemon', function(req,res){
   res.render('index.ejs',{
     pokemon: pokemon
   });
+})
+
+app.get('/pokemon/new', function(req,res){
+  res.render('new.ejs')
 })
 
 app.get('/pokemon/:id', function(req,res){
@@ -18,7 +25,15 @@ app.get('/pokemon/:id', function(req,res){
   });
 })
 
+app.post('/pokemon', function(req,res){
+  pokemon.push(req.body)
+  res.redirect('/pokemon')
+})
 
+app.delete('/pokemon/:id', function(req,res){
+  pokemon.splice(req.params.id,1);
+  res.redirect('/pokemon');
+})
 
 
 
@@ -37,3 +52,5 @@ const port = 3000
 app.listen(port, function() {
   console.log('servers up');
 })
+
+module.exports = app;
